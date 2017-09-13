9400f33042f38492abdcf9e3e405e0d014a85321
강진규 <takearib@naver.com>
2017-09-13 20:46:03 +0900

    AdminController.java 생성 AdminController.java - admin() 메소드 생성 WEB-INF/config/tiles/layout2.jsp 생성 WEB-INF/config/tiles/tiles.xml 수정 WEB-INF/jsp/admin/admin.jsp 폴더와 파일 생성 WEB-INF/jsp/tiles/admin_topMenu.jsp 생성 WEB-INF/jsp/tiles/admin_footer.jsp 생성 /src/main/webapp/index.jsp 내용 수정

--- tourMOA2.com/src/main/java/tourMOA/web/AdminController.java
+++ tourMOA2.com/src/main/java/tourMOA/web/AdminController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class MainController {
+public class AdminController {
 	
-	@RequestMapping("/main.do")
-	public String main() throws Exception{		
-		return "main/main";		
+	@RequestMapping("/admin.do")
+	public String admin() {
+		return "admin/admin";
 	}
-	
+
 }
---
+++ tourMOA2.com/src/main/webapp/WEB-INF/jsp/tiles/admin_footer.jsp
@@ -0,0 +1,4 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"  pageEncoding="UTF-8"%>
+<h2>admin_footer</h2>
+</body>
+</html>
