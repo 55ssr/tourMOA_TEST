c3a3755962da4e84600db767e9c823a4d80aeb75
김국진 <Administrator@EZEN-14-05-02>
2017-09-19 22:00:51 +0900

    footer부문 상세페이지(고객센터 부문만 남았음)

--- tourMOA2.com/src/main/java/tourMOA/web/CommonController.java
+++ tourMOA2.com/src/main/java/tourMOA/web/CommonController.java
@@ -7,43 +7,55 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CommonController {
 	
 	/*여행자보험*/
-	@RequestMapping("/insu01.do")
+	@RequestMapping("common/insu01.do")
 	public String insu01() throws Exception{
 		return "common/insu01";
 	}
 	
 	/*보상기준*/
-	@RequestMapping("/insu02.do")
+	@RequestMapping("common/insu02.do")
 	public String insu02() throws Exception{
 		return "common/insu02";
 	}
 	
 	/*보험약관*/
-	@RequestMapping("/insu03.do")
+	@RequestMapping("common/insu03.do")
 	public String insu03() throws Exception{
 		return "common/insu03";
 	}
 	
 	/*인터넷회원약관*/
-	@RequestMapping("/internetAgree.do")
+	@RequestMapping("common/internetAgree.do")
 	public String internetAgree() throws Exception{
 		return "common/internetAgree";
 	}
 	
+	/*개인정보처리방침*/
+	@RequestMapping("common/privacy.do")
+	public String privacy() throws Exception{
+		return "common/privacy";
+	}
+	
 	/*신문광고*/
-	@RequestMapping("/paperCalendar.do")
+	@RequestMapping("common/paperCalendar.do")
 	public String paperCalendar() throws Exception{
 		return "common/paperCalendar";
 	}
 	
 	/*사이트맵*/
-	@RequestMapping("/siteMap.do")
+	@RequestMapping("common/siteMap.do")
 	public String siteMap() throws Exception{
 		return "common/siteMap";
 	}
 	
+	/*고객센터 메인*/
+	@RequestMapping("common/main.do")
+	public String main() throws Exception{
+		return "common/main";
+	}
+	
 	/*여행약관*/
-	@RequestMapping("/travelAgree.do")
+	@RequestMapping("common/travelAgree.do")
 	public String travelAgree() throws Exception{
 		return "common/travelAgree";
 	}
