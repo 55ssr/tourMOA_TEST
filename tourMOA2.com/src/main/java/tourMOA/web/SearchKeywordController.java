6f41d39f0473729b1333c3024ac76f011771b156
kimkimhyeonji <ksesur4140@naver.com>
2017-10-17 21:37:53 +0900

    searchKeyword

--- tourMOA2.com/src/main/java/tourMOA/web/SearchKeywordController.java
+++ tourMOA2.com/src/main/java/tourMOA/web/SearchKeywordController.java
@@ -1,5 +1,8 @@
 package tourMOA.web;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -24,42 +27,21 @@ public class SearchKeywordController {
 	
 	/*키워드 검색부분*/
 	@RequestMapping("search/searchKeyword.do")
-	public String searchKeyword(@ModelAttribute("searchVO") DefaultListVO searchVO, Model model) throws Exception{
-
-		/*int recordCountPerPage = 10;
-		int pageSize = 5;
-		int totalCount = searchService.selectSearchKeywordTotal(searchVO);
-		int pageIndex = searchVO.getPageIndex(); // 사용자가 누른 페이지 값을 가져와야한다.
-
-		int firstPage = ((int) Math.floor((pageIndex - 1) / pageSize) * pageSize) + 1;
-		int lastPage = (firstPage + pageSize) - 1;
-
-		// 사용자가 1Page를 누른경우 1 , 2Pages 를 누른경우 11, 3Pages를 누른경우 31
-		// 5. 화면 출력할 행(데이터)의 시작 번호
-		int firstIndex = (pageIndex - 1) * 10 + 1;
-		// firstIndex가 1이면 10, 11이면 20, 21이면 30
-		// 5. 끝번호 (데이터베이스) recordCountPerPage 데이터 갯수
-		int lastIndex = (firstIndex + recordCountPerPage) - 1;
-
-		// 6. 총 페이지 개수
-		int totalPage = (int) Math.ceil((double) totalCount / recordCountPerPage);
-		// 7. [이전] / [다음] 처리할 변수 지정
-		int before = 0; // 링크 없음
-		if (firstPage > 1)
-			before = 1;
-		int next = 0; // 링크 없음
-		if (lastPage <= totalPage)
-			next = 1;
-
-		// 8. 행번호
+	public String searchKeyword(@ModelAttribute("searchVO") DefaultListVO searchVO
+			, Model model) throws Exception{
+		int viewCount = 20;
+		int recordCountPerPage = viewCount;
+		int firstIndex=1;
+		int lastIndex=(firstIndex + recordCountPerPage) - 1;
+		GregorianCalendar today = new GregorianCalendar();
+		int year = today.get ( Calendar.YEAR );
+		int month = today.get(Calendar.MONTH ) + 1;
+		
+		/*String yearNmonth = year+"년 "+month+"월";*/
+			
 		searchVO.setFirstIndex(firstIndex);
-		searchVO.setLastIndex(lastIndex); // 계산한값을 VO에 태워 새로 셋팅을 해준다.
-*/
-		/*int total = searchService.selectSearchKeywordTotal(searchVO);*/
-
-		// 사용자가 1Page를 보고있으면 total 값, 2Pages를 보고있으면 total-10, 3Pages는 total-20
-		/*int topNumber = total - (pageIndex - 1) * 10;*/
-
+		searchVO.setLastIndex(lastIndex);
+		
 		boolean tfKeyword = false;
 		String existKeyword = searchVO.getSearchKeyword();
 		String existCondition = searchVO.getSearchCondition();
@@ -69,7 +51,7 @@ public class SearchKeywordController {
 			tfKeyword=true;
 			
 		}
-		System.out.println(tfKeyword+"==="+existKeyword+"==="+existCondition);
+	
 		
 		if(tfKeyword==true){
 			boolean viewOpen = true;
@@ -83,21 +65,10 @@ public class SearchKeywordController {
 			boolean viewOpen = false;
 			model.addAttribute("viewOpen", viewOpen);
 		}
+		model.addAttribute("year", year);
+		model.addAttribute("month", month);
+		model.addAttribute("today", today);
 		
-		
-
-		/*model.addAttribute("recordCountPerPage", recordCountPerPage);
-		model.addAttribute("totalCount", totalCount);
-		model.addAttribute("firstPage", firstPage);
-		model.addAttribute("lastPage", lastPage);
-		
-
-		model.addAttribute("totalPage", totalPage);
-		model.addAttribute("before", before);
-		model.addAttribute("next", next);*/
-
-		
-		/*model.addAttribute("number", topNumber);*/
 		return "search/searchKeyword";
 	}
 	
